090620607d2a2e41bd9b0ec24e2e652f30bd19ea
That Random <andrewwoan@gmail.com>
2023-03-11 08:27:24 -0500

    basic preloader

--- app/Experience/Preloader.js
+++ app/Experience/Preloader.js
@@ -1,6 +1,11 @@
 import * as THREE from "three";
 import Experience from "./Experience.js";
 
+import lerp from "./Utils/functions/lerp.js";
+import elements from "./Utils/functions/elements.js";
+
+import gsap from "gsap";
+
 export default class Preloader {
     constructor() {
         this.experience = new Experience();
@@ -9,16 +14,40 @@ export default class Preloader {
         this.loaded = 0;
         this.queue = 0;
 
-        this.setPreloader();
+        this.previousProgress = 0;
 
-        this.resources.on("loading", (loaded, queue) => {
-            this.updateProgress(loaded, queue);
+        this.elements = elements({ loadingText: ".loading-text" });
+
+        // **** This is for updating a percentage ****
+        // this.resources.on("loading", (loaded, queue) => {
+        //     this.updateProgress(loaded, queue);
+        // });
+
+        this.resources.on("ready", () => {
+            this.playIntro();
         });
     }
 
-    setPreloader() {}
+    // setPreloader() {}
+    // updateProgress(loaded, queue) {
+    //     const percent = loaded / queue;
+    //     this.elements.loadingText.innerText = `${Math.round(percent * 100)}%`;
+    // }
 
-    updateProgress(loaded, queue) {
-        console.log(loaded, queue);
+    async playIntro() {
+        return new Promise((resolve) => {
+            this.timeline = new gsap.timeline();
+            this.timeline.to(".preloader", {
+                // opacity: 0.2,
+                top: "-100%",
+                delay: 1,
+                ease: "power4.out",
+                onComplete: () => {
+                    document
+                        .querySelector(".preloader")
+                        .classList.add("hidden");
+                },
+            });
+        });
     }
 }
