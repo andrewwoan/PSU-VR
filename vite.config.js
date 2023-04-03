79b30d4a0c429f53b575f94ca8e41fd90fb6d405
That Random <andrewwoan@gmail.com>
2023-04-02 20:39:56 -0500

    fix config

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,25 @@ export default defineConfig({
     build: {
         outDir: "../dist",
         emptyOutDir: true,
+        chunkSizeWarningLimit: 1600,
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes("node_modules")) {
+                        return id
+                            .toString()
+                            .split("node_modules/")[1]
+                            .split("/")[0]
+                            .toString();
+                    }
+                },
+            },
+        },
     },
     optimizeDeps: {
         include: ["socket.io-client"],
     },
+
     // server: {
     //     proxy: {
     //         "/foo": "http://localhost:3000",
